4719ab35013a9ce8640809a409d3924e941405d8
felixpieber <77886263+felixpieber@users.noreply.github.com>
2021-01-26 11:56:30 +0100

    Update Dark.Instagram.user.js

--- projects/Downloads/Dark.Instagram.user.js
+++ projects/Downloads/Dark.Instagram.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Instagram Dark Mode
+// @name         Dark Instagram
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @icon         https://i.imgur.com/re3cZAe.gif
 // @description  A Dark Mode Script for Instagram.
 // @author       Felix Pieber
