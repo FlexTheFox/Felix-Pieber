54ed8ed61bb939e3ee5322db5c579602d8872ad0
felixpieber <77886263+felixpieber@users.noreply.github.com>
2021-02-18 14:08:43 +0100

    tw points

---
+++ projects/Downloads/Twitch.Points.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Twitch Points
+// @namespace    http://tampermonkey.net/
+// @version      0.9.2
+// @description  Auto clicking "Channel Points" on Twicht.tv
+// @author       Felix Pieber
+// @match        https://www.twitch.tv/*
+// @run-at       document-end
+// @icon         https://i.imgur.com/Jks7wvmg.png
+// @website      https://felixpieber.github.io/web/#!/
+// @website      https://www.instagram.com/flex_the_fox/
+// @updateURL    https://felixpieber.github.io/web/projects/Downloads/Twitch.Points.user.js/
+// @grant        none
+// ==/UserScript==
+
+const button = '.claimable-bonus__icon';
+var onMutate = function(mutationsList) {
+	mutationsList.forEach(mutation => {
+		if(document.querySelector(button)) document.querySelector(button).click();
+	})
+}
+var observer = new MutationObserver(onMutate);
+observer.observe(document.body, {childList: true, subtree: true});
